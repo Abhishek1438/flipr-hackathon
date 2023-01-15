90c14aff3d90d74eca4ffc476d19235a3fade772
tushar <tusharph1@gmail.com>
2023-01-15 21:41:02 +0530

    added icons

--- src/pages/register.js
+++ src/pages/register.js
@@ -14,7 +14,10 @@ const Register = () => {
     event.preventDefault();
     fetch('https://flipr-2e7c.onrender.com/register', {
       method: 'POST',
-      body: { email, password },
+      body: { username: email, password },
+      headers: {
+        'Content-type': 'x-www-form-urlencoded'
+      },
     }).then((response) => response.json());
 
     router.push(`/dashboard/${email}`);
@@ -55,12 +58,12 @@ const Register = () => {
             <p className={classes.orPara}>or</p>
             <Link href="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Fflipr-2e7c.onrender.com%2Fsecret&scope=profile&client_id=1022897147206-dc1jkoih9etntvkq13r1ve27tntdgihc.apps.googleusercontent.com&service=lso&o2v=2&flowName=GeneralOAuthFlow">
               <p>
-                Signup with <span>Google</span>
+                <FaGoogle /> Signup with <span>Google</span>
               </p>
             </Link>
             <Link href="https://github.com/login?client_id=3713ed9956c94ba511fc&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D3713ed9956c94ba511fc%26redirect_uri%3Dhttps%253A%252F%252Fflipr-2e7c.onrender.com%252Fauth%252Fgithub%252Fsecret%26response_type%3Dcode">
               <p>
-                Signup with <span>Github</span>
+                <FaGithub /> Signup with <span>Github</span>
               </p>
             </Link>
           </div>
