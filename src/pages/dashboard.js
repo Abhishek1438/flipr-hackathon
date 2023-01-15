c45d18e687942a46e03456eff26bd2e35b2e6d73
Abhishek1438 <abhishekverma1438@gmail.com>
2023-01-15 12:06:55 +0530

    dashboard added

---
+++ src/pages/dashboard.js
@@ -0,0 +1,80 @@
+import Navbar from '@/components/Navbar';
+import Range from '@/components/Range';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import OptionBar from '@/components/OptionBar';
+import { useEffect, useState } from 'react';
+import Overview from '@/components/Overview';
+import Chart from '@/components/Cart';
+import styles from '../styles/dashboard.module.css';
+
+const Dashboard = () => {
+  const [option, setOption] = useState('');
+  const date = new Date();
+  const day = date.getUTCDate();
+  const month = date.getMonth() + 1;
+  const year = date.getUTCFullYear();
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const newDate = `${day}/${month}/${year} ${hours}:${minutes}`;
+
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  };
+
+  const [selected, setSelected] = useState('Overview');
+
+  const [selectedComponent, setSelectedComponent] = useState(<Overview />);
+  useEffect(() => {
+    if (selected === 'Overview') {
+      setSelectedComponent(<Overview />);
+    } else if (selected === 'Chart') {
+      setSelectedComponent(<Chart />);
+    } else setSelectedComponent(<p>Working on it</p>);
+  }, [selected]);
+
+  return (
+    <main>
+      <Navbar />
+      <h2>NIFTY 50</h2>
+      <hr />
+      <div className={styles.tradeDataContainer}>
+        <div className={styles.tradeData}>
+          <h1>{17172.15}</h1>
+          <h2 style={{ color: 'green' }}>2,32,434(0.234%)</h2>
+          <p>As on {newDate}</p>
+        </div>
+        <div className={styles.tradeDataRange}>
+          <Range type={'Day'} current={17172.15} lowest={17774.25} highest={17976.4} />
+          <Range type={'52 week'} current={17172.15} lowest={15183.4} highest={18887.6} />
+          <h4 style={{ color: 'rgb(176, 176, 176)' }}>Returns</h4>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <FormControl sx={{ m: 1, minWidth: 80 }}>
+              <InputLabel id="demo-simple-select-label">YTD</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={option}
+                label={'Option'}
+                onChange={handleChange}
+              >
+                <MenuItem value={'YTD'}>YTD</MenuItem>
+                <MenuItem value={'YML'}>YML</MenuItem>
+                <MenuItem value={'XYZ'}>XYZ</MenuItem>
+              </Select>
+            </FormControl>
+            <p>3.55%</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <OptionBar setSelectedComponent={setSelected} />
+      <hr style={{ margin: 0 }} />
+      {selectedComponent}
+    </main>
+  );
+};
+
+export default Dashboard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,7 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
-import Navbar from '@/components/Navbar';
-import Range from '@/components/Range';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import { useEffect, useState } from 'react';
-import OptionBar from '@/components/OptionBar';
-import Overview from '@/components/Overview';
-import Chart from '@/components/Cart';
 
 export default function Home() {
-  const [option, setOption] = useState('');
-  const date = new Date();
-  const day = date.getUTCDate();
-  const month = date.getMonth() + 1;
-  const year = date.getUTCFullYear();
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-  const newDate = `${day}/${month}/${year} ${hours}:${minutes}`;
-
-  const handleChange = (event) => {
-    setOption(event.target.value);
-  };
-
-  const [selected, setSelected] = useState('Overview');
-
-  const [selectedComponent, setSelectedComponent] = useState(<Overview />);
-  useEffect(() => {
-    if (selected === 'Overview') {
-      setSelectedComponent(<Overview />);
-    } else if (selected === 'Chart') {
-      setSelectedComponent(<Chart />);
-    } else setSelectedComponent(<p>Working on it</p>);
-  }, [selected]);
-
   return (
     <>
       <Head>
@@ -44,44 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Navbar />
-        <h2>NIFTY 50</h2>
-        <hr />
-        <div className={styles.tradeDataContainer}>
-          <div className={styles.tradeData}>
-            <h1>{17172.15}</h1>
-            <h2 style={{ color: 'green' }}>2,32,434(0.234%)</h2>
-            <p>As on {newDate}</p>
-          </div>
-          <div className={styles.tradeDataRange}>
-            <Range type={'Day'} current={17172.15} lowest={17774.25} highest={17976.4} />
-            <Range type={'52 week'} current={17172.15} lowest={15183.4} highest={18887.6} />
-            <h4 style={{ color: 'rgb(176, 176, 176)' }}>Returns</h4>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <FormControl sx={{ m: 1, minWidth: 80 }}>
-                <InputLabel id="demo-simple-select-label">YTD</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={option}
-                  label={'Option'}
-                  onChange={handleChange}
-                >
-                  <MenuItem value={'YTD'}>YTD</MenuItem>
-                  <MenuItem value={'YML'}>YML</MenuItem>
-                  <MenuItem value={'XYZ'}>XYZ</MenuItem>
-                </Select>
-              </FormControl>
-              <p>3.55%</p>
-            </div>
-          </div>
-        </div>
-        <hr />
-        <OptionBar setSelectedComponent={setSelected} />
-        <hr style={{ margin: 0 }} />
-        {selectedComponent}
-      </main>
+      <main className={styles.main}>complete it</main>
     </>
   );
 }
