77a250e8c8e0507cfab369aaae1fc9a1991aeb3e
tushar <tusharph1@gmail.com>
2023-01-16 20:13:28 +0530

    added loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,3 +1,4 @@
+import { height } from '@mui/system'
 import styles from './Loading.module.css'
 
 
@@ -7,7 +8,9 @@ export default function Loading() {
     return (
         <div className='mx-auto w-fit text-center my-3' style={{
             display: "flex",
-            justifyContent: "center"
+            justifyContent: "center",
+            minHeight: "80vh",
+            alignItems: "center"
         }}>
             <div class={styles.sk_chase}>
                 <div class={styles.sk_chase_dot}></div>
